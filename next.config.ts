b2b1d697e9e6b8182454f96f000996bbf02d4f8b
monkeyband114 <gadocer54@gmail.com>
2024-11-01 15:57:58 +0100

    Changes to be committed: 	modified:   next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/dinsite",
   output: "export", // <=== enables static exports
   reactStrictMode: true,
 };
